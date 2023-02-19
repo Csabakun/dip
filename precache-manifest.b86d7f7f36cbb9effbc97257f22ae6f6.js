bf3d2aceca1f5b087517004cdb99548a407d068b
Gerencsér Csaba <csabiger@gmail.com>
2023-02-19 11:58:30 +0100

    Updates

---
+++ precache-manifest.b86d7f7f36cbb9effbc97257f22ae6f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb7cf5cfd68b0a470227",
+    "url": "/dip/static/css/main.eb2c87a4.chunk.css"
+  },
+  {
+    "revision": "cb7cf5cfd68b0a470227",
+    "url": "/dip/static/js/main.cb7cf5cf.chunk.js"
+  },
+  {
+    "revision": "9bcc136cc792575690c6",
+    "url": "/dip/static/css/1.79ddfbbd.chunk.css"
+  },
+  {
+    "revision": "9bcc136cc792575690c6",
+    "url": "/dip/static/js/1.9bcc136c.chunk.js"
+  },
+  {
+    "revision": "9caa4197d97570ccb16f",
+    "url": "/dip/static/js/runtime~main.9caa4197.js"
+  },
+  {
+    "revision": "7494ea050cb798c9cc748f2494dfe41c",
+    "url": "/dip/static/media/test.7494ea05.jpg"
+  },
+  {
+    "revision": "c8ea0ee3b1b4e98d7b1a1bd51074c43e",
+    "url": "/dip/index.html"
+  }
+];
